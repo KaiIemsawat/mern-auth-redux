72905cbf9105696cd4ee588a20d775e81a7597c7
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-29 09:27:05 -0400

    sestup base routes

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 
 import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const port = process.env.PORT || 8000;
 
 const app = express();
 app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => res.send("server is ready"));
 
