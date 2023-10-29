40daf4013f138822df8e3a0fbb2aa1f5e2e11522
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-29 10:54:09 -0400

    jwt authentication login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,20 @@ import genToken from "../utils/generateToken.js";
 // route        - POST /api/users/auth
 // @access      - Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Auth User" });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+        genToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid Credentials");
+    }
 });
 
 // @desc        - Register new user
