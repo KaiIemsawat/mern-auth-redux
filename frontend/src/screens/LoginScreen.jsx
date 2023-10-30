3a83e43375bfd6829b2e6ff3935bed5810a1a9e3
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-30 09:28:53 -0400

    add toast, login works

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,16 +1,50 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 import FormContainer from "../components/FormContainer";
+import { useLoginMutation } from "../slices/userApiSlice";
+import { setCredentials } from "../slices/authSlice";
 
 const LoginScreen = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    // const [justNameOfFunctionToCall, {isLoading}] = useLoginMutation();
+    // {isLoading} is there by default
+    const [login, { isLoading }] = useLoginMutation();
+
+    // To get user data
+    // 'userInfo' comes from 'authSlice.js'
+    // 'auth' from '(state) => state.auth' also comes from 'authSlice.js'
+    const { userInfo } = useSelector((state) => state.auth);
+
+    // If user is logged in
+    useEffect(() => {
+        if (userInfo) {
+            navigate("/");
+        }
+    }, [navigate, userInfo]);
+
     const submitHandler = async (e) => {
         e.preventDefault();
-        console.log("submit");
+        try {
+            // calling 'login'
+            // 'unwrap()' will return 'promise'
+            const res = await login({ email, password }).unwrap();
+            // setting user to 'state' and 'local storage'
+            dispatch(setCredentials({ ...res }));
+            // navigate to home screen
+            navigate("/");
+        } catch (error) {
+            toast.error(error?.data?.message || error.error);
+            console.error(error?.data?.message || error.error);
+        }
     };
 
     return (
